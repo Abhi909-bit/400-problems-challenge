4214ea81abab978913f923a4b481922fd4b12daf
Abhishek Kumar <52343738+Abhi909-bit@users.noreply.github.com>
2022-07-16 13:39:13 +0530

    Add files via upload

---
+++ 50)Why strings are immutable in Java.cpp
@@ -0,0 +1,4 @@
+// Problem Statement : Why strings are immutable in Java?
+
+//Being immutable automatically makes the String thread safe since they won't be changed when accessed from multiple threads.
+ //Hence immutable objects, in general, can be shared across multiple threads running simultaneously.
